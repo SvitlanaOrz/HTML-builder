6dadea51a087257205fc0e2cb4f5d8ff354ada17
SvitlanaOrz <svitlana.orz@gmail.com>
2023-05-08 21:40:36 +0300

    fix: fix deleted files functionality

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,26 +1,22 @@
 const fs = require("fs");
 const path = require("path");
 
-const sourceFolderPath = "./04-copy-directory/files";
-const destinationFolderPath = "./04-copy-directory/files-copy";
-
-// копирование папки
-function copyDir(source, destination) {
-  fs.mkdir(destination, { recursive: true }, (err) => {
+function copyDir(sourceDir, destinationDir) {
+  fs.mkdir(destinationDir, { recursive: true }, (err) => {
     if (err) {
-      console.error("Ошибка считывания папки:", err);
+      console.error("ошибка считывания папки:", err);
       return;
     }
 
-    fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    fs.readdir(sourceDir, { withFileTypes: true }, (err, files) => {
       if (err) {
-        console.error("Ошибка записи папки:", err);
+        console.error("ошибка считывания папки:", err);
         return;
       }
 
       files.forEach((file) => {
-        const sourceFilePath = path.join(source, file.name);
-        const destinationFilePath = path.join(destination, file.name);
+        const sourceFilePath = path.join(sourceDir, file.name);
+        const destinationFilePath = path.join(destinationDir, file.name);
 
         if (file.isDirectory()) {
           copyDir(sourceFilePath, destinationFilePath);
@@ -28,25 +24,55 @@ function copyDir(source, destination) {
           copyFile(sourceFilePath, destinationFilePath);
         }
       });
+
+      // удаление файлов
+      fs.readdir(
+        destinationDir,
+        { withFileTypes: true },
+        (err, destinationFiles) => {
+          if (err) {
+            console.error("ошибка считывания папки:", err);
+            return;
+          }
+
+          const destinationFileNames = destinationFiles.map(
+            (file) => file.name
+          );
+
+          destinationFiles.forEach((file) => {
+            const sourceFilePath = path.join(sourceDir, file.name);
+            const destinationFilePath = path.join(destinationDir, file.name);
+
+            if (!fs.existsSync(sourceFilePath)) {
+              fs.unlink(destinationFilePath, (err) => {
+                if (err) {
+                  console.error("ошибка считывания файла:", err);
+                }
+              });
+            }
+          });
+        }
+      );
     });
   });
 }
 
-// укопирования файла
-function copyFile(source, destination) {
-  const readStream = fs.createReadStream(source);
-  const writeStream = fs.createWriteStream(destination);
+function copyFile(sourceFile, destinationFile) {
+  const readStream = fs.createReadStream(sourceFile);
+  const writeStream = fs.createWriteStream(destinationFile);
 
   readStream.on("error", (err) => {
-    console.error("Ошибка считывания файла:", err);
+    console.error("ошибка считывания файла:", err);
   });
 
   writeStream.on("error", (err) => {
-    console.error("Ошибка записи файла:", err);
+    console.error("Пошибка записи файла:", err);
   });
 
   readStream.pipe(writeStream);
 }
 
-// запуск функции копирования файла
-copyDir(sourceFolderPath, destinationFolderPath);
+const sourceDir = "./04-copy-directory/files";
+const destinationDir = "./04-copy-directory/files-copy";
+
+copyDir(sourceDir, destinationDir);
