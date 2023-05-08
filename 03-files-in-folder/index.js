332b784f103276955f30ab6dc43727122012f743
SvitlanaOrz <svitlana.orz@gmail.com>
2023-05-08 21:20:35 +0300

    fix: delete synk method from task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -5,7 +5,7 @@ const folderPath = "./03-files-in-folder/secret-folder";
 // получаем информацию о файлах в папке secret-folder
 fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
   if (err) {
-    console.error("Помилка зчитування папки:", err);
+    console.error("Ошибка считывания папки:", err);
     return;
   }
 
@@ -16,16 +16,18 @@ fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
   filteredFiles.forEach((file) => {
     const fileName = file.name;
     const fileExtension = path.extname(fileName).slice(1);
-    const fileSize = getFileSize(path.join(folderPath, fileName));
 
-    console.log(`${fileName} - ${fileExtension} - ${fileSize}`);
+    fs.stat(path.join(folderPath, fileName), (err, stats) => {
+      if (err) {
+        console.error("Помилка отримання інформації про файл:", err);
+        return;
+      }
+
+      const fileSizeInKb = stats.size / 1024;
+
+      console.log(
+        `${fileName} - ${fileExtension} - ${fileSizeInKb.toFixed(3)}kb`
+      );
+    });
   });
 });
-
-// получение размеров файлов вкилобайтах
-function getFileSize(filePath) {
-  const stats = fs.statSync(filePath);
-  const fileSizeInBytes = stats.size;
-  const fileSizeInKb = fileSizeInBytes / 1024;
-  return `${fileSizeInKb.toFixed(3)}kb`;
-}
